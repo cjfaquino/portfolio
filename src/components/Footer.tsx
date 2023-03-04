957420e378baee5150b419f3b353c12ae2640fd7
Christian Aquino <cjfaquino@gmail.com>
2023-03-03 22:36:09 -0800

    add Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
+function Footer() {
+  return (
+    <footer>
+      <span>Copyright © 2023</span>{' '}
+      <a href='https://github.com/cjfaquino'>Christian Aquino</a> <GitHubIcon />
+    </footer>
+  );
+}
+
+export default Footer;
