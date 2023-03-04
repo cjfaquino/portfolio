a0c151f804affc722e5a4b855fde032954918ee5
Christian Aquino <cjfaquino@gmail.com>
2023-03-03 22:10:26 -0800

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import autoprefixer from 'autoprefixer';
+import postcssPresetEnv from 'postcss-preset-env';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+  },
+  css: {
+    postcss: {
+      plugins: [autoprefixer, postcssPresetEnv()],
+    },
+  },
+});
