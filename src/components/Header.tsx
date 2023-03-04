2464bfa012b741b4ccc75c7860e91fbbc06fb280
Christian Aquino <cjfaquino@gmail.com>
2023-03-03 22:19:37 -0800

    add Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>Christian Aquino</li>
+          <li>Home</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
