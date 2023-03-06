d5a5a00f26fa55c147c7ec47860f929da32b6b2e
Christian Aquino <cjfaquino@gmail.com>
2023-03-05 21:53:05 -0800

    add contact button & remove tech stack

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,23 +1,58 @@
-import React from 'react';
-import TechStack from './TechStack';
+import React, { useEffect, useRef } from 'react';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
 
 function AboutMe() {
+  const btnRef = useRef(null);
+  const divRef = useRef(null);
+
+  const email = 'Christian@ChristianAquino.net';
+  const mailLink = `mailto:${email}`;
+
+  useEffect(() => {
+    // show/hide small contact button
+    if (divRef) {
+      const options = {
+        rootMargin: '100px',
+        threshold: 1,
+      };
+
+      const scrollObserver = new IntersectionObserver((entries) => {
+        const entry = entries[0];
+        (btnRef.current! as Element).classList.toggle(
+          'show',
+          !entry.isIntersecting
+        );
+      }, options);
+
+      scrollObserver.observe(divRef.current!);
+    }
+  }, [divRef]);
+
   return (
     <section>
       <h2>About Me</h2>
       <div className='about-me grid'>
         <p>
-          {`It makes me proud to be able to create beautiful code. Whether it be a
-          passion project or commissioned project. With technology constantly
-          changing, I enjoy learning better ways to improve on my existing
-          knowledge on UI/UX, web development. Outside of web development, I
-          enjoy tinkering with computers, cars, motorcycles.`}
+          {`I take immense pride in crafting beautiful code, be it for my personal passion projects or commissioned work. In today's constantly evolving technological landscape, I am constantly seeking out new and innovative ways to enhance my knowledge of UI/UX and web development, in order to provide my clients with the best possible outcome.`}
         </p>
+
         <p>
-          {`Well I wouldn't be a very good developer if I hadn't made my own site! If you would like to see some of my projects, feel free to take a look at my portfolio, or get in touch.`}
+          {`Beyond web development, I love mechanical keyboards! The tactile feel and sound of the keys are so satisfying. There are many types of switches and keycaps to personalize your keyboard. It's a fun hobby, whether building a custom keyboard or just swapping keycaps. Add some personality to your workspace!`}
         </p>
-        <TechStack />
+
+        <div className='flex-col' ref={divRef}>
+          <p>
+            {`Well I wouldn't be a very good developer if I hadn't made my own site! If you would like to see some of my projects, feel free to take a look at my portfolio, or get in touch.`}
+          </p>
+          <a href={mailLink} className='contact flex'>
+            Contact me <MailOutlineIcon />
+          </a>
+        </div>
       </div>
+
+      <a href={mailLink} className='contact small flex' ref={btnRef}>
+        <MailOutlineIcon />
+      </a>
     </section>
   );
 }
