67792e9bcc61486f0f08180028066edefa45ac92
Christian Aquino <cjfaquino@gmail.com>
2023-03-09 20:59:34 -0800

    add interface Project

---
+++ src/interfaces/Project.ts
@@ -0,0 +1,10 @@
+export interface Project {
+  name: string;
+  path: string;
+  live: string;
+  repo: string;
+  desc: string;
+  laptopIMG: string;
+  phoneIMG?: string;
+  tabletIMG?: string;
+}
