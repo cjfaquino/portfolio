bc1740000da4c853d70d7b293f2c7f97237a7f4e
Christian Aquino <cjfaquino@gmail.com>
2023-03-10 22:00:11 -0800

    add prev/next & change window title

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -1,39 +1,74 @@
 import LaunchIcon from '@mui/icons-material/Launch';
 import GitHub from '@mui/icons-material/GitHub';
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import React, { useEffect, useLayoutEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
 import projects from '../assets/projects.json';
 import Laptop from '../components/Laptop';
 import Phone from '../components/Phone';
 import Tablet from '../components/Tablet';
+import { Project as IProject } from '../interfaces/Project';
 
 function Project() {
   const params = useParams();
-  const projectPath = params.project;
-  const found = projects.find((p) => p.path === projectPath);
+  const [project, setProject] = useState<IProject | null>(null);
+  const [index, setIndex] = useState<number | null>(null);
 
-  return found ? (
+  useEffect(() => {
+    const projectPath = params.project;
+    // get project from params
+    if (projectPath) {
+      const i = projects.findIndex((p) => p.path === projectPath);
+      setProject(projects[i] as IProject);
+      setIndex(i);
+    }
+
+    return () => {};
+  }, [params.project]);
+
+  useLayoutEffect(() => {
+    // set window title
+    if (project) {
+      document.title = `${project.name} | Christian Aquino`;
+    }
+    return () => {
+      document.title = 'Front End Developer | Christian Aquino';
+    };
+  }, [project]);
+
+  return project && index !== null ? (
     <main>
       <section className='project'>
         <div className='container relative select-disable'>
-          <Laptop imgURL={found.laptopIMG} />
-          <Tablet imgURL={found.tabletIMG} />
-          <Phone imgURL={found.phoneIMG} />
+          <div className='arrows absolute flex'>
+            <Link to={`/${projects.at(index - 1)!.path}`}>
+              <ArrowBackIosNewIcon fontSize='inherit' />{' '}
+              {projects.at(index - 1)!.name}
+            </Link>
+            <Link to={`/${projects.at((index + 1) % projects.length)!.path}`}>
+              {projects.at((index + 1) % projects.length)!.name}{' '}
+              <ArrowForwardIosIcon fontSize='inherit' />
+            </Link>
+          </div>
+          <Laptop imgURL={project.laptopIMG} />
+          <Tablet imgURL={project.tabletIMG} />
+          <Phone imgURL={project.phoneIMG} />
         </div>
         <span className='flex'>
-          <h2>{found.name} </h2>
+          <h2>{project.name} </h2>
           <div className='links flex'>
-            <a href={found.repo}>
+            <a href={project.repo}>
               github <GitHub fontSize='inherit' />
             </a>{' '}
-            <a href={found.live}>
+            <a href={project.live}>
               live <LaunchIcon fontSize='inherit' />
             </a>
           </div>
         </span>
 
         <p>
-          {found.desc ||
+          {project.desc ||
             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor possimus facere ab. A magnam quasi facilis sed quae, nesciunt tenetur vero itaque similique porro, placeat totam fuga sapiente! Error, ipsa.'}
         </p>
       </section>
